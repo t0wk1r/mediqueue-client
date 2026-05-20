40971ba09ebea2aa3e55cbe616eecad57673374b
t0wk1r <techtowkir@gmail.com>
2026-05-21 01:13:33 +0600

    About , Contact and FAQ page title dynamic

---
+++ app/faq/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FAQ",
+};
+
+export default function FaqLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
